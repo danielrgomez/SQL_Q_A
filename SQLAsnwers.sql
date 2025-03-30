d0c67f169025eec2ca296c1ea2da5f46f00e1025
danielrgomez <40222049+danielrgomez@users.noreply.github.com>
2025-03-30 13:56:52 -0400

    Second Commit

--- SQLAsnwers.sql
+++ SQLAsnwers.sql
@@ -1,4 +1,33 @@
---Question 1
+--Question 11
+--a
 Select count(NationalIDNumber) as Number_Of_Employees from HumanResources.Employee;
 
---Question 2
+--b
+Select count(NationalIDNumber) as Number_Of_Employees from HumanResources.Employee where CurrentFlag = 1;
+
+--c
+Select Count(HRE.JobTitle) Count_Of_SP_Person_Type from HumanResources.Employee as HRE Inner Join Person.Person as PP on HRE.BusinessEntityID = PP.BusinessEntityID Where PP.PersonType = 'SP' Group BY PP.PersonType;
+
+--d
+Select Count(HRE.JobTitle) Count_Of_SP_Person_Type 
+from HumanResources.Employee as HRE 
+Inner Join Person.Person as PP 
+on HRE.BusinessEntityID = PP.BusinessEntityID
+Inner Join HumanResources.EmployeeDepartmentHistory as HREDH
+on HRE.BusinessEntityID = HREDH.BusinessEntityID
+Inner Join HumanResources.Department HRD on
+HREDH.DepartmentID = HRD.DepartmentID
+where PP.PersonType = 'SP' and HRD.Name = 'Sales' group by  PP.PersonType;
+
+
+
+
+select * from HumanResources.Department;
+
+
+
+Select top (5) * from Person.Person where PersonType = 'SP';
+
+--d
+
+Select top (5) * from HumanResources.Employee;
